f157c0aeb0b9802e34e26368cae702c662560405
công lê xuân <congle.190904@gmail.com>
2025-09-07 22:25:08 +0700

    init project chat real time with spring boot with websocket Tomp

---
+++ src/main/java/com/example/chat_real_time/ChatRealTimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.chat_real_time;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatRealTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatRealTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chat_real_time/ChatRealTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chat_real_time;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatRealTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
